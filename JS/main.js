fbeec33a61efda07be35205365bea1795ea3aa14
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2023-05-28 11:21:13 -0700

    new update

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+﻿//contact
+function copyFunction() {
+    // Get the text field
+    var copyText = document.getElementById("emailId").innerHTML;
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+
+    // Alert the copied text
+    alert('"' + copyText + '" was copied to your clipboard');
+}
